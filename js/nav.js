ee3522f18a48402e7a94f110e62fa03ca27ddd6b
Marcelo A.G. Toledo <marcelo.toledogt@gmail.com>
2023-10-24 16:47:05 -0300

    Limpeza de codigo e mudança de nomes

--- js/nav.js
+++ js/nav.js
@@ -1,67 +1,38 @@
-/*
-document.addEventListener("DOMContentLoaded", 
-function(){
-    const menus = this.getElementsByClassName("categoria");
-    alert("pagina carregada");
-    for(let i=0; i < menus.length; i++){
-        const submenus = menus[i].getElementsByClassName("subcategoria")
-        
-        menus[i].addEventListener("click", 
-        function(){
-            menus.classList.toggle("active");
-            if(submenus.style.display === none){
-                submenus.style.display = block;
-            }else{
-                submenus.style.display = none;
-            }
-            
-        });
-
-        for (let j=0; j<submenus.length; j++){
-            submenus[j].addEventListener("click", 
-            function(){
-
-            });
-        }
-    }
-});
-*/
 
 document.addEventListener("DOMContentLoaded", 
 function(){
     const menus = this.getElementsByClassName("categoria");
     
-
     for(let i = 0; i < menus.length; i++){
         //alert("for:" + menus.length);
-        let item = menus[i].getElementsByClassName("item-categoria");
-        let submenus = menus[i].getElementsByClassName("subcategoria");
-        let nomeSubmenu = menus[i].getElementsByClassName("nome-subcategoria") ;
-        let itensSubmenus = menus[i].getElementsByClassName("item-subcategoria");
+        let nomeCategoria = menus[i].getElementsByClassName("nome-categoria");
+        let subCategoria = menus[i].getElementsByClassName("subcategoria");
+        let nomeSubCategoria = menus[i].getElementsByClassName("nome-subcategoria") ;
+        let itensSubCategoria = menus[i].getElementsByClassName("item-subcategoria");
 
-        item[0].addEventListener("click", 
+        nomeCategoria[0].addEventListener("click", 
         function(){ 
-            for(let j = 0; j < submenus.length; j++){
+            for(let j = 0; j < subCategoria.length; j++){
                 
-                if(submenus[j].style.display === "block"){
-                    submenus[j].style.display = "none";
+                if(subCategoria[j].style.display === "block"){
+                    subCategoria[j].style.display = "none";
                 }
                 else{
-                    submenus[j].style.display = "block";
+                    subCategoria[j].style.display = "block";
                 }
             }
 
-            for(let j = 0; j < submenus.length; j++){
-                let nome = nomeSubmenu[j];
+            for(let j = 0; j < subCategoria.length; j++){
+                let nome = nomeSubCategoria[j];
                 
-                if(submenus[j].contains(itensSubmenus[j])){
+                if(subCategoria[j].contains(itensSubCategoria[j])){
                     nome.addEventListener("click", 
                     function(){
-                        if(itensSubmenus[j].style.display === "block"){
-                            itensSubmenus[j].style.display = "none";
+                        if(itensSubCategoria[j].style.display === "block"){
+                            itensSubCategoria[j].style.display = "none";
                         }
                         else{
-                            itensSubmenus[j].style.display = "block";
+                            itensSubCategoria[j].style.display = "block";
                         }
                     });
                 }
@@ -70,14 +41,8 @@ function(){
             }
             
         });
-
-       // alert(itensSubmenus.length + " : " + itensSubmenus[0].innerHTML);
-        
-        
         
     }
-
-
 
 });
 
