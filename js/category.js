87d1ee7ff46d447816e083d93708cbbf17deb3c1
Marcelo A.G. Toledo <marcelo.toledogt@gmail.com>
2023-12-02 13:33:51 -0300

    sigle page: feito navegação

---
+++ js/category.js
@@ -0,0 +1,48 @@
+
+document.addEventListener("DOMContentLoaded", 
+function(){
+    const menu = this.getElementsByClassName("category");
+    
+    for(let i = 0; i < menu.length; i++){
+        //alert("for:" + menus.length);
+        let categoryName = menu[i].getElementsByClassName("categoryName");
+        let subcategory = menu[i].getElementsByClassName("subcategory");
+        let subcategoryName = menu[i].getElementsByClassName("subcategoryName") ;
+        let subcategory2 = menu[i].getElementsByClassName("subcategory2");
+
+        categoryName[0].addEventListener("click", 
+        function(){ 
+            for(let j = 0; j < subcategory.length; j++){
+                
+                if(subcategory[j].style.display === "flex"){
+                    subcategory[j].style.display = "none";
+                }
+                else{
+                    subcategory[j].style.display = "flex";
+                }
+            }
+
+            for(let j = 0; j < subcategory.length; j++){
+                let name = subcategoryName[j];
+                
+                if(subcategory[j].contains(subcategory2[j])){
+                    name.addEventListener("click", 
+                    function(){
+                        if(subcategory2[j].style.display === "flex"){
+                            subcategory2[j].style.display = "none";
+                        }
+                        else{
+                            subcategory2[j].style.display = "flex";
+                        }
+                    });
+                }
+
+
+            }
+            
+        });
+        
+    }
+
+});
+
--- js/nav.js
+++ js/nav.js
@@ -1,48 +1,323 @@
+const containerNavegation = document.getElementById("containerNavegation");
 
-document.addEventListener("DOMContentLoaded", 
-function(){
-    const menu = this.getElementsByClassName("category");
+document.addEventListener("DOMContentLoaded", function() {
+
+    // barra inicial================================================
+    const navHomeBar = document.createElement("nav");
+    navHomeBar.id = "homeBar";
     
-    for(let i = 0; i < menu.length; i++){
-        //alert("for:" + menus.length);
-        let categoryName = menu[i].getElementsByClassName("categoryName");
-        let subcategory = menu[i].getElementsByClassName("subcategory");
-        let subcategoryName = menu[i].getElementsByClassName("subcategoryName") ;
-        let subcategory2 = menu[i].getElementsByClassName("subcategory2");
+    
+        //logo
+        const divLogo = document.createElement("div");
+        divLogo.id = "logo";
+        
+            //aLogo
+            const aLogo = document.createElement("a");
+            
+                //picture
+                const pictureLogo = document.createElement("picture");
 
-        categoryName[0].addEventListener("click", 
-        function(){ 
-            for(let j = 0; j < subcategory.length; j++){
+                    //imageLogo
+                    const imageLogo = document.createElement("img");
+                    imageLogo.src = "../../images/nav/logo/logo_apenas_icone.png";
+                    imageLogo.width = "50";
+                    imageLogo.height = "50";
+    
+
+        //pesquisa
+        const divSearch = document.createElement("div");
+        divSearch.id = "homeSearch";
+            //input
+            const inputBarHome = document.createElement("input");
+            //button
+            const buttonSearch = document.createElement("button");
+            buttonSearch.textContent = "Buscar";
+        
+        //login e carrinho
+        const divLoginCart = document.createElement("div");
+        divLoginCart.id = "loginCart";
+            //aLogin
+            const aLogin = document.createElement("a");
+                const imageLogin = document.createElement("img");
+                imageLogin.id = "login";
+                imageLogin.src = "../../images/nav/login/login.png";
+                imageLogin.width = "50";
+                imageLogin.height = "50";
+            //aCart
+            const aCart = document.createElement("a");
+                const imageCart = document.createElement("img");
+                imageCart.id = "cart"
+                imageCart.src = "../../images/nav/cart/shopping-cart.png";
+                imageCart.width = "50";
+                imageCart.height = "50";
                 
-                if(subcategory[j].style.display === "flex"){
-                    subcategory[j].style.display = "none";
+        
+        //menu
+        const divMenu = document.createElement("div");
+        divMenu.id = "menu";
+            const buttonMenu = document.createElement("button");
+            buttonMenu.onclick = function openMenu(){
+    
+                let categoryNavigation = document.getElementById("myTopnav");
+            
+                if(categoryNavigation.className === "categoryNavigation"){
+                    categoryNavigation.className = "categoryNavigationHidden"
+                }else{
+                    categoryNavigation.className = "categoryNavigation"
                 }
-                else{
-                    subcategory[j].style.display = "flex";
+            
+                let loginCartMobile = document.getElementById("myLoginCartTopnav");
+            
+                if(loginCartMobile.className === "loginCartMobile"){
+                    loginCartMobile.className = "loginCartMobileHidden"
+                }else{
+                    loginCartMobile.className = "loginCartMobile"
                 }
-            }
-
-            for(let j = 0; j < subcategory.length; j++){
-                let name = subcategoryName[j];
-                
-                if(subcategory[j].contains(subcategory2[j])){
-                    name.addEventListener("click", 
-                    function(){
-                        if(subcategory2[j].style.display === "flex"){
-                            subcategory2[j].style.display = "none";
-                        }
-                        else{
-                            subcategory2[j].style.display = "flex";
-                        }
-                    });
-                }
-
-
             }
             
-        });
-        
+                const imageMenu = document.createElement("img");
+                imageMenu.src = "../../images/nav/menu/menu_margin.png";
+                imageMenu.width = "50";
+                imageMenu.height = "50";
+
+    //==
+    //containerNavegation
+    containerNavegation.appendChild(navHomeBar);
+
+    navHomeBar.appendChild(divLogo);
+    divLogo.appendChild(aLogo);
+    aLogo.appendChild(pictureLogo);
+    pictureLogo.appendChild(imageLogo);
+
+    navHomeBar.appendChild(divSearch);
+    divSearch.appendChild(inputBarHome);
+    divSearch.appendChild(buttonSearch);
+
+    navHomeBar.appendChild(divLoginCart);
+    divLoginCart.appendChild(aLogin);
+    divLoginCart.appendChild(aCart);
+    aLogin.appendChild(imageLogin)
+    aCart.appendChild(imageCart);
+
+    navHomeBar.appendChild(divMenu);
+    divMenu.appendChild(buttonMenu);
+    buttonMenu.appendChild(imageMenu);
+
+
+
+    //========================================================
+    //= barra para mobile
+
+    const navMobileBar = document.createElement("nav");
+    navMobileBar.classList.add("loginCartMobile");
+    navMobileBar.id = "myLoginCartTopnav";
+
+        const aLoginMobile = document.createElement("a");
+        aLoginMobile.classList.add("linkLoginMobile");
+
+            const imgLoginMobile = document.createElement("img");
+            imgLoginMobile.id = "loginMobile";
+            imgLoginMobile.src = "../../images/nav/login/login_white.png";
+            imgLoginMobile.width = "50";
+            imgLoginMobile.height = "50";
+
+        const aCartMobile = document.createElement("a");
+        aCartMobile.classList.add("linkCartMobile");
+
+            const imgCartMobile = document.createElement("img");
+            imgCartMobile.id = "cartMobile";
+            imgCartMobile.src = "../../images/nav/cart/shopping-cart_white.png";
+            imgCartMobile.width = "50";
+            imgCartMobile.height = "50";
+
+    //==
+    //containerNavegation
+    containerNavegation.appendChild(navMobileBar);
+    navMobileBar.appendChild(aLoginMobile);
+    navMobileBar.appendChild(aCartMobile);
+    aLoginMobile.appendChild(imgLoginMobile);
+    aCartMobile.appendChild(imgCartMobile);
+
+    //========================================================
+    //= barra de categoria
+
+    const navCategoryNavigation = document.createElement("nav");
+    navCategoryNavigation.classList.add("categoryNavigation");
+    navCategoryNavigation.id = "myTopnav";
+    navCategoryNavigation.innerHTML = `
+    <div class="column">
+          <ul class="category">
+            <li class="categoryName categoryItem">CAFE</li>
+
+            <ul class="subcategory">
+              <li class="subcategoryName categoryItem">POR MÉTODO</li>
+
+              <ul class="subcategory2">
+                <li class="item categoryItem"><a href="#">Grãos</a></li>
+                <li class="item categoryItem"><a href="#">Moídos</a></li>
+              </ul>
+            </ul>
+
+            <ul class="subcategory">
+              <li class="subcategoryName categoryItem">INTENSIDADE</li>
+
+              <ul class="subcategory2">
+                <li class="item categoryItem"><a href="#">Suave</a></li>
+                <li class="item categoryItem"><a href="#">Média</a></li>
+                <li class="item categoryItem"><a href="#">Intensa</a></li>
+              </ul>
+            </ul>
+
+            <ul class="subcategory">
+              <li class="subcategoryName categoryItem">TIPO</li>
+
+              <ul class="subcategory2">
+                <li class="item categoryItem"><a href="#">Aromatizado</a></li>
+                <li class="item categoryItem"><a href="#">Certificado</a></li>
+              </ul>
+            </ul>
+
+          </ul>
+        </div>
+    
+        <div class="column">
+          <ul class="category">
+            <li class="categoryName categoryItem">CAFETEIRAS</li>
+
+            <ul class="subcategory">
+              <li class="subcategoryName categoryItem">MÁQUINAS</li>
+
+              <ul class="subcategory2">
+                <li class="item categoryItem"><a href="#">Máquinas Para Grãos</a></li>
+                <li class="item categoryItem"><a href="#">Máquinas Para Pó</a></li>
+                <li class="item categoryItem"><a href="#">Máquinas Para Cápsulas</a></li>
+              </ul>
+
+            </ul>
+          </ul>
+        </div>
+    
+        <div class="column">
+          <ul class="category">
+            <li class="categoryName categoryItem">ACESSÓRIOS</li>
+
+            <ul class="subcategory">
+              <li class="subcategoryName categoryItem">MOEDORES</li>
+
+              <ul class="subcategory2">
+                <li class="item categoryItem"><a href="#">Manuais</a></li>
+                <li class="item categoryItem"><a href="#">Elétricos</a></li>
+              </ul>
+            </ul>
+
+            <ul class="subcategory">
+              <li class="subcategoryName categoryItem"><a href="#">FILTRO DE PAPEL</a></li>
+            </ul>
+
+            <ul class="subcategory">
+              <li class="subcategoryName categoryItem"><a href="#">CHALEIRAS</a></li>
+            </ul>
+
+          </ul>
+        </div>
+    
+        <div class="column">
+          <ul class="category">
+            <li class="categoryName categoryItem">BEBIDAS & GOURMET</li>
+
+            <ul class="subcategory">
+              <li class="subcategoryName categoryItem">BEBIDAS</li>
+                
+              <ul class="subcategory2">
+                <li class="item categoryItem"><a href="#">Cappuccinos</a></li>
+              </ul>
+            </ul>
+
+
+            <ul class="subcategory">
+              <li class="subcategoryName categoryItem">ACOMPANHAMENTO</li>
+              
+              <ul class="subcategory2">
+                <li class="item categoryItem"><a href="#">Chocolates</a></li>
+              </ul>
+            </ul>
+
+            <ul class="subcategory">
+              <li class="subcategoryName categoryItem"><a href="#">CHAROPES</a></li>
+            </ul>
+
+          </ul>
+        </div>
+    
+        <div class="column">
+          <ul class="category">
+            <li class="categoryName categoryItem">MAIS</li>
+
+            <ul class="subcategory">
+              <li class="subcategoryName categoryItem">USUÁRIO</li>
+
+              <ul class="subcategory2">
+                <li class="item categoryItem"><a href="userPersonalData.html">Informações do Usuário</a></li>
+                <li class="item categoryItem"><a href="userPurchaseHistory.html">Histórico de compra</a></li>
+              </ul>
+            </ul>
+
+            <ul class="subcategory">
+              <li class="subcategoryName categoryItem">INSTITUCIONAL</li>
+
+              <ul class="subcategory2">
+                <li class="item categoryItem"><a href="#">Quem Somos</a></li>
+              </ul>
+            </ul>
+
+            <ul class="subcategory">
+              <li class="subcategoryName categoryItem">SUPORTE E AJUDA</li>
+
+              <ul class="subcategory2">
+                <li class="item categoryItem"><a href="#">Central de Atendimento</a></li>
+                <li class="item categoryItem"><a href="#">Frete e Promoções</a></li>
+              </ul>
+            </ul>
+
+          </ul>
+        </div>
+    `
+    containerNavegation.appendChild(navCategoryNavigation);
+  });
+     
+  /**
+   *     for (const item of product) {
+        let divProduct = document.createElement("div");
+        divProduct.classList.add("product");
+    
+        let imgProduct = document.createElement("img");
+        imgProduct.src = item.imageSrc;
+        imgProduct.alt = "Preencher";
+    
+        let h2Product = document.createElement("h2");
+        h2Product.textContent = item.name;
+    
+        let pProduct = document.createElement("p");
+        pProduct.textContent = item.price
+
+        let divBnt = document.createElement("div");
+        divBnt.classList.add("btnProduct");
+    
+        let btnProductCart = document.createElement("button");
+        btnProductCart.textContent = "Adicionar ao Carrinho";
+    
+        let btnProductBuy = document.createElement("button");
+        btnProductBuy.textContent = "Comprar Agora"
+    
+        divBnt.appendChild(btnProductCart);
+        divBnt.appendChild(btnProductBuy);
+
+        divProduct.appendChild(imgProduct);
+        divProduct.appendChild(h2Product);
+        divProduct.appendChild(pProduct);
+        divProduct.appendChild(divBnt);
+
+        productContainer.appendChild(divProduct);
+
     }
-
-});
-
+   */
